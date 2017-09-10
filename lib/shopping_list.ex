d88ca6ec352bd6513e7e2608cb40c06e9a7caecf
Sasa Juric <sasa.juric@gmail.com>
2017-09-10 21:55:44 +0200

    add basic ShoppingList abstraction

--- lib/shopping_list.ex
+++ lib/shopping_list.ex
@@ -1,2 +1,30 @@
 defmodule ShoppingList do
+  # -------------------------------------------------------------------
+  # API
+  # -------------------------------------------------------------------
+
+  def new(), do:
+    []
+
+  def entries(shopping_list), do:
+    shopping_list
+
+  def size(shopping_list), do:
+    length(shopping_list)
+
+  def add_entry(shopping_list, name, quantity) do
+    new_entry = new_entry(name, quantity)
+    [new_entry | shopping_list]
+  end
+
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp new_entry(name, quantity), do:
+    %{
+      name: name,
+      quantity: quantity
+    }
 end
--- test/shopping_list_test.exs
+++ test/shopping_list_test.exs
@@ -1,3 +1,39 @@
 defmodule ShoppingListTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+
+  # -------------------------------------------------------------------
+  # Tests
+  # -------------------------------------------------------------------
+
+  test "empty list" do
+    list = ShoppingList.new()
+
+    assert ShoppingList.size(list) == 0
+    assert ShoppingList.entries(list) == []
+  end
+
+  test "one-element list" do
+    list =
+      ShoppingList.new()
+      |> ShoppingList.add_entry("eggs", 12)
+
+    assert ShoppingList.size(list) == 1
+    assert ShoppingList.entries(list) == [%{name: "eggs", quantity: 12}]
+  end
+
+  test "two-elements list" do
+    list =
+      ShoppingList.new()
+      |> ShoppingList.add_entry("biers", 6)
+      |> ShoppingList.add_entry("eggs", 12)
+
+    assert ShoppingList.size(list) == 2
+    sorted_entries = list |> ShoppingList.entries() |> Enum.sort_by(&(&1.name))
+    assert sorted_entries ==
+      [
+        %{name: "biers", quantity: 6},
+        %{name: "eggs", quantity: 12}
+      ]
+  end
 end
